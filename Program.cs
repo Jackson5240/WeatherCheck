b3a5c8e041478d898fadcb41ec2e72242eb7ae8e
Jackson5240 <74751585+Jackson5240@users.noreply.github.com>
2025-10-06 15:01:35 +0800

    Update Program.cs
    Added Commets to Program.cs

--- Program.cs
+++ Program.cs
@@ -5,15 +5,21 @@ namespace WeatherForecastProj
 {
     public class Program
     {
+        // ------------------------------------------------------------
+        // Entry point of the UENValidateProj web application.
+        // This is where the ASP.NET Core runtime begins execution.
+        // ------------------------------------------------------------
         public static void Main(string[] args)
         {
+        // Build and start the web host using the configuration below
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args)   // Load default host settings (appsettings.json, environment vars, etc.)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Specify the Startup class to define services and middleware.
                     webBuilder.UseStartup<Startup>();
                 });
     }
